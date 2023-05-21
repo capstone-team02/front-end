977782eb8d7edf75b72aa56aaf957001bbbf1c82
jeeewonkim <wldnjs3865@naver.com>
2023-05-21 20:33:48 +0900

    feat: mypage css 완성

--- src/Router.tsx
+++ src/Router.tsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
       },
       {
         path: "mypage",
-        element: <Mypage />,
+        //element: <Mypage />,
         children: [
           {
             path: "",
