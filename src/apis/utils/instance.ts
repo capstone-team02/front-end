ac2f63cfc1cb615454d43f52a9edb44dda34f77d
jeeewonkim <wldnjs3865@naver.com>
2023-06-27 21:47:32 +0900

    feat: BASE_URL 수정

--- src/apis/utils/instance.ts
+++ src/apis/utils/instance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const BASE_URL = "https://newproject-327406.du.r.appspot.com/mytown/v1";
+const BASE_URL = "https://integral-vim-327406.et.r.appspot.com/mytown/v1";
 //const BASE_URL = "http://localhost:8080/mytown/v1";
 
 const axiosConfig: AxiosRequestConfig = {
